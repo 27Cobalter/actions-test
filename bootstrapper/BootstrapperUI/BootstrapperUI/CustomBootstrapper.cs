c90287c2d513808a3c8ae8a1e01612de5f9333b8
27Cobalter <kontyanzhihg+github@gmail.com>
2025-01-09 00:07:27 +0900

    Bundle Installer

---
+++ bootstrapper/BootstrapperUI/BootstrapperUI/CustomBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Threading;
+using WixToolset.Mba.Core;
+
+namespace BootstrapperUI
+{
+    public class CustomBootstrapper : BootstrapperApplication
+    {
+        private readonly IEngine _engine;
+        public CustomBootstrapper(IEngine engine) : base(engine) {
+            _engine = engine;
+        }
+
+        protected override void Run()
+        {
+            var dispatcher = Dispatcher.CurrentDispatcher;
+            var w = new MainView();
+            w.Closed += (_, __) => dispatcher.InvokeShutdown();
+
+            _engine.Detect();
+            w.Show();
+            Dispatcher.Run();
+            _engine.Quit(0);
+        }
+    }
+}
---
+++ bootstrapper/BootstrapperUI/BootstrapperUI/WpfBaFactory.cs
@@ -0,0 +1,16 @@
+﻿using BootstrapperUI;
+using WixToolset.Mba.Core;
+
+[assembly: BootstrapperApplicationFactory(typeof(WpfBaFactory))]
+
+namespace BootstrapperUI
+{
+    public class WpfBaFactory : BaseBootstrapperApplicationFactory
+    {
+        protected override IBootstrapperApplication Create(IEngine engine, IBootstrapperCommand bootstrapperCommand)
+        {
+            var ba = new CustomBootstrapper(engine);
+            return ba;
+        }
+    }
+}
